be2cbdd88e89f11185161e815dd1878a0909532f
Alumno <alumno@iMac-de-IT-Admin.local>
2019-02-07 20:27:51 -0600

    Add passwor And mail fields

--- Pinterest/ViewController.swift
+++ Pinterest/ViewController.swift
@@ -17,17 +17,36 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
         view.addSubview(inputContainerV)
         inputContainerV.addSubview(nameTextf)
+        inputContainerV.addSubview(Mailtf)
+        inputContainerV.addSubview(PassWordTF)
+        
+        nameTextf.topAnchor.constraint(equalTo: inputContainerV.topAnchor).isActive = true
+        nameTextf.heightAnchor.constraint(equalTo: inputContainerV.heightAnchor, multiplier: 1/3).isActive = true
+        nameTextf.widthAnchor.constraint(equalTo: inputContainerV.widthAnchor).isActive = true
+        nameTextf.leftAnchor.constraint(equalTo: inputContainerV.leftAnchor).isActive = true
+        
+        Mailtf.topAnchor.constraint(equalTo: nameTextf.bottomAnchor).isActive = true
+        Mailtf.heightAnchor.constraint(equalTo: inputContainerV.heightAnchor, multiplier: 1/3).isActive = true
+        Mailtf.widthAnchor.constraint(equalTo: inputContainerV.widthAnchor).isActive = true
+        Mailtf.leftAnchor.constraint(equalTo: inputContainerV.leftAnchor).isActive = true
+        
+        PassWordTF.topAnchor.constraint(equalTo: Mailtf.bottomAnchor).isActive = true
+        PassWordTF.heightAnchor.constraint(equalTo: inputContainerV.heightAnchor, multiplier: 1/3).isActive = true
+        PassWordTF.widthAnchor.constraint(equalTo: inputContainerV.widthAnchor).isActive = true
+        PassWordTF.leftAnchor.constraint(equalTo: inputContainerV.leftAnchor).isActive = true
         
         inputContainerV.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         inputContainerV.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        inputContainerV.heightAnchor.constraint(equalToConstant: 150)
+        inputContainerV.heightAnchor.constraint(equalToConstant: 150).isActive = true
         inputContainerV.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30).isActive = true
         
         
+        
         view.addSubview(fButton)
-        fButton.topAnchor.constraint(equalTo: inputContainerV.bottomAnchor, constant: 20)
+        fButton.topAnchor.constraint(equalTo: inputContainerV.bottomAnchor, constant: 20).isActive = true
         fButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        fButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30).isActive = true
+        fButton.leftAnchor.constraint(equalTo: inputContainerV.leftAnchor).isActive = true
+        fButton.rightAnchor.constraint(equalTo: inputContainerV.rightAnchor).isActive = true
     }
     let nameTextf : UITextField = {
        let tf = UITextField()
@@ -35,6 +54,22 @@ class ViewController: UIViewController {
         tf.placeholder = "Name"
         tf.backgroundColor = .white
         return tf
+    }()
+    let Mailtf : UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Email"
+        tf.backgroundColor = .white
+        return tf
+    }()
+    let PassWordTF : UITextField = {
+        let tf = UITextField()
+        tf.isSecureTextEntry = true
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Password"
+        tf.backgroundColor = .white
+        return tf
+        
     }()
     
     let inputContainerV: UIView = {
@@ -49,8 +84,15 @@ class ViewController: UIViewController {
         ub.backgroundColor = UIColor(red: 81/255, green: 101/255, blue: 161/255, alpha: 1)
         ub.setTitle("Register", for: .normal)
         ub.translatesAutoresizingMaskIntoConstraints = false
+        ub.addTarget(self, action: #selector(handleButton), for: .touchUpInside)
         return ub
     }()
+    
+    @objc func handleButton (){
+        let N : String? = nameTextf.text
+        let pass : String? = PassWordTF.text
+        let mail :String? = Mailtf.text
+    }
     
 }
 
